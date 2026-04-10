2bacb807d55aee11e934cf2c5b32fec1946dd228
sahilkhan86306145-lgtm <sahilkhan86306145@gmail.com>
2026-04-10 21:58:19 +0530

    update dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,15 @@
 export default function Dashboard() {
   return (
-    <div>
-      Dashboard Ready 🚀
+    <div style={{ padding: 20 }}>
+      <h1>Welcome TITANFLOWFINANCE 🚀</h1>
+
+      <h3>Dashboard</h3>
+
+      <ul>
+        <li><a href="/dashboard/deposit">Deposit</a></li>
+        <li><a href="/dashboard/withdraw">Withdraw</a></li>
+        <li><a href="/dashboard/kyc">KYC</a></li>
+      </ul>
     </div>
   );
 }
