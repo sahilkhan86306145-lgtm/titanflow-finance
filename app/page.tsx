a000bf58273a15b3508bff486c8b086ceca57063
sahilkhan86306145-lgtm <sahilkhan86306145@gmail.com>
2026-04-10 22:47:47 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,3 @@
 export default function Home() {
-  return (
-    <main style={{padding: "20px", fontFamily: "sans-serif"}}>
-      <h1>🔥 Titanflow Finance</h1>
-      <p>Site is working ✅</p>
-
-      <a href="/dashboard">Go to Dashboard</a>
-    </main>
-  );
+  return <h1>Working ✅</h1>;
 }
- 
